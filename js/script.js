9c6702cf70779e911182d6780bdccfe3eabcbcb6
MuliroMatt <murilo.mattiuzzi@gmail.com>
2024-01-26 19:01:10 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.innerHTML += input;
+}
+
+function clearDisplay(){
+    display.innerHTML = "";
+}
+
+function calculate(){
+    try{
+        display.innerHTML = eval(display.innerHTML);
+    }
+    catch(error){
+        display.innerHTML = "Error"
+    }
+}
+
+
+
